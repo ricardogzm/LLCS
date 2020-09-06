c4289ebcbd9aa8c6cf2a1cb4340aaec050eac47d
EdgarRMed <edgarrmedinalozano@gmail.com>
2020-09-05 19:25:17 -0500

    Se eliminaron los casos no necesarios

--- main.c
+++ main.c
@@ -7,10 +7,6 @@ void insertarAlFinal(int d);
 
 void mostrar();
 
-int contarDato(int dato);
-
-void intercambio(int a, int b);
-
 //Definimos nuestros Nodos
 typedef struct nodo {
     int dato;
@@ -18,7 +14,7 @@ typedef struct nodo {
 } Nodo;
 
 //declarar el inicio de la lista
-Nodo *inicio = NULL;  //lista vacia
+Nodo * inicio = NULL;  //lista vacia
 
 int main() {
     int op, dato, kk;
@@ -29,9 +25,6 @@ int main() {
         printf("1. Mostrar elementos de la lista \n");
         printf("2. Insertar un elemento al inicio de la lista \n");
         printf("3. Insertar un elemento al final de la lista\n");
-        printf("4. Contar el número de veces que un dato X aparece en la lista\n");
-        printf("5. Determinar si un elemento X existe o no\n");
-        printf("6. Sustituir un dato X por uno Y en la lista\n");
         printf("0. Salir\n");
         printf("Opcion: ");
         scanf("%d", &op);
@@ -48,27 +41,6 @@ int main() {
                 printf("Ingrese elemento a incertar al final\n");
                 scanf("%d", &dato);
                 insertarAlFinal(dato);
-                break;
-            case 4:
-                printf("Ingrese elemento a contar\n");
-                scanf("%d", &dato);
-                printf("El elemento se encontro %d veces\n", contarDato(dato));
-                break;
-            case 5:
-                printf("Ingrese elemento a corroborar su existencia en la lista\n");
-                scanf("%d", &dato);
-                if (contarDato(dato) == 0) {
-                    printf("El elemento no se encuentra en la lista\n");
-                } else {
-                    printf("El elemento se encuentra en la lista\n");
-                }
-                break;
-            case 6:
-                printf("Ingrese elemento a extraer de la lista\n");
-                scanf("%d", &dato);
-                printf("Ingrese elemento a incertar de la lista\n");
-                scanf("%d", &kk);
-                intercambio(dato, kk);
                 break;
             case 0:
                 printf("Bonito día\n");
@@ -129,38 +101,5 @@ void mostrar() {
         printf("\n");
     }
 
-}
-
-int contarDato(int dato) {
-    int cont = 0;
-    Nodo *aux;
-    aux = inicio;
-    while (aux != NULL) {
-        if (aux->dato == dato) {
-            cont++;
-        }
-        aux = aux->sig;
-    }
-    return cont;
-}
-
-void intercambio(int x, int y) {
-    if (inicio == NULL) { //lista vacia
-        printf("Sin elementos\n");
-    } else {
-        if (contarDato(x) != 0) {
-            Nodo *aux;
-            aux = inicio;
-            while (aux != NULL) {
-                if (aux->dato == x) {
-                    aux->dato = y;
-                }
-                aux = aux->sig;
-            }
-        } else {
-            printf("%d no esta en la lista\n", x);
-        }
-
-    }
 }
 
