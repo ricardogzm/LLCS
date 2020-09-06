2bea3b0a88a689d04a5d1708d39c8df8475ae7a6
ricardogzm <ricardo.gzm.g@gmail.com>
2020-09-05 23:32:12 -0500

    Se agregó lectura de archivos y archivo de prueba

--- main.c
+++ main.c
@@ -5,24 +5,25 @@
 void insertar(int d, char *N);
 void eliminarI();
 void eliminarF();
-
-
 void mostrar();
+void leerArchivo(char *nombreArchivo);
+
+#define NOMBRE_MAX 100
 
 //Definimos nuestros Nodos
 typedef struct nodo {
     int tiempo;
-    char nombre[100];
+    char nombre[NOMBRE_MAX];
     struct nodo *sig;
 } Nodo;
 
 //declarar el inicio de la lista
-Nodo * inicio = NULL;  //lista vacia
-Nodo * final = NULL;   //lista vacia
+Nodo *inicio = NULL;  //lista vacia
+Nodo *final = NULL;   //lista vacia
 
 int main() {
     int op, tiempo;
-    char nombre[100];
+    char nombre[NOMBRE_MAX];
 
     printf("Programa que inserta datos en una lista ligada\n");
     do {
@@ -31,17 +32,20 @@ int main() {
         printf("2. Insertar un elemento al inicio de la lista \n");
         printf("3. Eliminar el inicio\n");
         printf("4. Eliminar el final\n");
+        printf("5. Leer desde archivo\n");
         printf("0. Salir\n");
-        printf("Opcion: ");
+        printf("> ");
         scanf("%d", &op);
         getchar();
+        printf("\n");
+
         switch (op) {
             case 1:
                 mostrar();
                 break;
             case 2:
                 printf("Ingrese el nombre\n");
-                fgets( nombre, 100, stdin );
+                fgets(nombre, NOMBRE_MAX, stdin );
                 nombre[strcspn(nombre, "\n")] = 0; // Busca y elimina el salto de linea
                 printf("Ingrese el tiempo de ejecucion\n");
                 scanf("%d", &tiempo);
@@ -53,13 +57,20 @@ int main() {
             case 4:
                 eliminarF();
                 break;
+            case 5:
+                printf("Ingresar nombre de archivo: ");
+                fgets(nombre, NOMBRE_MAX, stdin);
+                nombre[strcspn(nombre, "\n")] = 0;
+                leerArchivo(nombre);
+                break;
             case 0:
-                printf("Bonito día\n");
+                printf("Adios.\n");
                 break;
             default:
                 printf("Opcion invalida \n");
         }
     } while (op != 0);
+
     return 0;
 }
 
@@ -82,7 +93,6 @@ void insertar(int d, char *N) {
     }
 }
 
-
 void mostrar() {
     if (inicio == NULL) {  //lista vacia solo se inserta nodo
         printf("Lista vacia\n");
@@ -93,32 +103,33 @@ void mostrar() {
         do {
             printf("Proceso: %s, Tiempo: %d, -> ",aux->nombre, aux->tiempo);
             aux = aux->sig;
-        } while (aux != inicio);
+        } while (aux != final);
+        printf("Proceso: %s, Tiempo: %d",aux->nombre, aux->tiempo);
 
         printf("\n");
     }
 }
 
-void eliminarI(){
+void eliminarI() {
     Nodo *aux;
-    if(inicio==NULL)
+    if (inicio == NULL)
         printf("Lista vacia\n");
-    else{
-        if(inicio==final){
+    else {
+        if (inicio == final) {
             free(inicio);
             inicio = final = NULL;
-        } else{
+        } else {
             aux = inicio;
-            inicio = inicio -> sig;
-            final -> sig = inicio;
+            inicio = inicio->sig;
+            final->sig = inicio;
             free(aux);
         }
     }
 }
 
-void eliminarF(){
+void eliminarF() {
     Nodo *aux, *aux2;
-    if(inicio==NULL)
+    if(inicio == NULL)
         printf("Lista vacia\n");
     else{
         if(inicio==final){
@@ -134,4 +145,25 @@ void eliminarF(){
             free(aux);
         }
     }
+}
+
+void leerArchivo(char *nombreArchivo) {
+    FILE *f;
+    int tiempo;
+    char nombre[NOMBRE_MAX];
+
+    if ((f = fopen(nombreArchivo, "r")) == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    while (inicio != NULL) {
+        eliminarI();
+    }
+
+    while (fscanf(f, "%s %d", nombre, &tiempo) > 0) {
+        insertar(tiempo, nombre);
+    }
+
+    fclose(f);
 }
